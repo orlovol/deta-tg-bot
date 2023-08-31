f41e60730afb047ef3c91b03018ba8d1f1894718
Oleksandr Orlov <1079585+orlovol@users.noreply.github.com>
2023-08-31 14:27:04 +0300

    host

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ TOKEN = getenv("BOT_TOKEN")
 
 # Webserver settings
 # bind localhost only to prevent any external access
-WEB_SERVER_HOST = "127.0.0.1"
+WEB_SERVER_HOST = "0.0.0.0"
 # Port for incoming request from reverse proxy. Should be any available port
 WEB_SERVER_PORT = getenv("PORT", 8080)
 
